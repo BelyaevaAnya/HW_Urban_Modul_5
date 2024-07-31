8d2b44ffbc2571a88a0f59b5bbfe8049e98836e7
Anya_Belyaeva <71212278+BelyaevaAnya@users.noreply.github.com>
2024-07-31 11:30:23 +0300

    HW

---
+++ arrays.py
@@ -0,0 +1,17 @@
+from pack_sort.Practice1 import insertion_sort, bubble_sort, selection_sort
+
+data_1 = [9, 7, 8, 9, 0, 1, 11, 23, 5, 2, 1, 0, 234]
+data_2 = [9, 7, 8, 9, 0, 1]
+data_3 = [1, 45, 67, 0, 1213, 434, 3]
+
+print(insertion_sort(data_1))
+print(insertion_sort(data_2))
+print(insertion_sort(data_3))
+
+print(bubble_sort(data_1))
+print(bubble_sort(data_2))
+print(bubble_sort(data_3))
+
+print(selection_sort(data_1))
+print(selection_sort(data_2))
+print(selection_sort(data_3))
